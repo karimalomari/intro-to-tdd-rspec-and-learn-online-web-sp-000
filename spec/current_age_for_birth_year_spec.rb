4a01513c396d78fa2cc0d16f136fc741d771da0a
Karim Alomari <karim.alomari@gmail.com>
2020-07-03 14:13:09 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -4,6 +4,7 @@ describe "current_age_for_birth_year method" do
   it "returns the age of a person based on the year of birth" do
   def
   current_age_for_birth_year (birth_year)
+  2003 - birth_year
 end
     
   end
